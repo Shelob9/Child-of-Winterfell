613f0e2cde4f3594811c3be053fe5cfc2442525c
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-23 12:32:57 -0400

    added picture to home page

--- page.php
+++ page.php
@@ -3,7 +3,11 @@
 
     <!-- Main Content -->
     <div class="twelve columns" role="content">
-
+<div class="four columns">
+			<?php $src = site_url('/assets/sitting@goddard.jpg', 'http'); ?>
+			<img src="<?php echo $src; ?>" class="radi-only">
+		</div>
+		<div class="eight columns" >
 		<?php if ( have_posts() ) : ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -11,7 +15,7 @@
 			<?php endwhile; ?>
 			
 		<?php endif; ?>
-
+		</div>
     </div>
     <!-- End Main Content -->
 </div>

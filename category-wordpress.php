e2e60e0783b1b8db659a4b449e38f24e46ad0608
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-23 19:38:42 -0400

    working wordpress cat page

--- category-wordpress.php
+++ category-wordpress.php
@@ -5,12 +5,15 @@
     <div class="twelve columns" role="content">
 		<div class="row">
 			<div class="ten columns centered">
-				<p>Shit about WordpRess</p>
+				<p>Shit about WordPress</p>
 			</div>
 		</div>
 		<div class="row">
-			<div class="ten columns centered">
-				<?php get_template_part('slider'); ?>
+			<div class="four columns">
+				<p>Here are some WordPress theme customizations that I have done. Please send me an email if you'd like to talk about how I can help you design, deploy and manage a WordPress site.<strongalter this</strong></p>
+			</div>
+			<div class="eight columns centered">
+				<?php get_template_part('slider-sites'); ?>
 			</div>
 		</div>
 		

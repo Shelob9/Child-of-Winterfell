ae4221cdf41edf9ce4a558de462574920296887b
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-23 10:00:35 -0400

    added page.php

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+
+    <!-- Main Content -->
+    <div class="nine columns panel callout radius" role="content"  style="background:#D8D2B1; border:1px solid #799E65;">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
+			<?php endwhile; ?>
+			
+		<?php endif; ?>
+
+    </div>
+    <!-- End Main Content -->
+<div class="three columns panel">
+	<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>

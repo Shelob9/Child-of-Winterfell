892a2f6ef5affcb2a3f39a3d71d7e6f091bdc214
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-23 19:43:18 -0400

    footer in total -wrap class, adjust padding and margins

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
 <footer>
 	
 	<div class="row">
-		<div class=" ten columns centered">
+		<div class=" ten columns centered total-wrap" style="padding-top:4px; padding-bottom:4px;">
 			<div class="row">
 				<div class="two columns mobile-two" id="twitter">
 					<a href="https://twitter.com/Josh412" class="twitter-follow-button" data-show-count="false" data-size="large" data-dnt="true">Follow @Josh412</a>
@@ -34,7 +34,7 @@
 						<img src="<?php $src = get_stylesheet_directory_uri().'/stylesheets/images/fc-webicon-googleplus-m.png'; echo $src; ?>" >
 					</a>
 				</div>
-				<div class="two columns extra-button">
+				<div class="two columns extra-button" style="margin-top:4px; margin-right:14px;">
 <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
 <script type="IN/MemberProfile" data-id="http://www.linkedin.com/pub/josh-pollock/5/900/978" data-format="hover" data-text="Josh Pollock" data-related="false"></script>
 				</div>

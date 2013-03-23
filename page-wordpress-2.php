b3379e4f28915114afb57e42d57154e3006debe6
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-23 12:16:32 -0400

    layout for wordpress page

--- page-wordpress-2.php
+++ page-wordpress-2.php
@@ -3,12 +3,25 @@
 
     <!-- Main Content -->
     <div class="twelve columns" role="content">
-
-		<?php query_posts( 'cat=7' );
-			while ( have_posts() ) : the_post();
-				get_template_part( 'loop', 'entry' );
-			endwhile;
-		?>
+		<div class="row">
+			<div class="ten columns centered">
+				<p>Shit about WordpRess</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="ten columns centered">
+				<?php get_template_part('slider'); ?>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="twelve columns">
+				<?php query_posts( 'cat=7' );
+					the_post();
+						get_template_part( 'loop', 'entry' );
+				?>
+			</div>
+		</div>
 		
     </div>
     <!-- End Main Content -->

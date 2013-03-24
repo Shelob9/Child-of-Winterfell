1f121a92aa75befab1aa31a26d299ca3373a8968
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-24 02:45:49 -0400

    added download links to resume

--- resume_temp.php
+++ resume_temp.php
@@ -11,7 +11,7 @@ get_header(); ?>
 <div class="twelve columns" role="content">
 	<hgroup style="margin-bottom:45px;">
 		<div class="row" >
-			<div class="nine columns mobile-two">
+			<div class="six columns mobile-two">
 				<h1>Josh Pollock: Resume</h1>
 			</div>
 			<div class="one columns mobile-one" >
@@ -23,12 +23,46 @@ get_header(); ?>
 					<img src="<?php echo $t; ?>">
 				</a>
 			</div>
-			<div class="two columns mobile-one">
-				<a href="mailto:JPollock412@gmail.com" class="genericon genericon-mail" style="font-size:32px; margin-right:16px;"></a>
-				<a href="http://www.linkedin.com/pub/josh-pollock/5/900/978" class="genericon  genericon-linkedin-alt" style="font-size:32px;margin-right:16px;margin-left:16px;"></a>
-				<a href="/" class="genericon genericon-home" style="font-size:32px;margin-left:16px;"></a>
+			<div class="four columns mobile-one">
+				<div class="row" style="margin-bottom:16px;">
+					Contact:<br />
+					<a href="mailto:JPollock412@gmail.com" class="genericon genericon-mail" style="font-size:32px; margin-right:16px;"></a>
+					<a href="http://www.linkedin.com/pub/josh-pollock/5/900/978" class="genericon  genericon-linkedin-alt" style="font-size:32px;margin-right:16px;margin-left:16px;"></a>
+					<a href="/" class="genericon genericon-home" style="font-size:32px;margin-left:16px;"></a>
+					</div>
+					<div class="row">
+						Download Resume:<br />
+						<?php
+						$link = site_url('/assets/JPollockResume.odt');
+						$bef = "<img src=\"";
+						$aft = "\">";
+						$src = get_stylesheet_directory_uri().'/stylesheets/images/odt.png';
+						$icon = $bef.$src.$aft;
+						?>
+						<a href="<?php $odt; ?>" title="Download as Open Office Text Document">
+							<?php echo $icon; ?>
+						</a>
+						<?php
+						$link = site_url('/assets/JPollockResume.doc');
+						$src = get_stylesheet_directory_uri().'/stylesheets/images/doc.png';
+						$icon = $bef.$src.$aft;
+						?>
+						<a href="<?php $link; ?>" title="Download as Word Doc">
+							<?php echo $icon; ?>
+						</a>
+						<?php
+						$link = site_url('/assets/JPollockResume.pdf');
+						$src = get_stylesheet_directory_uri().'/stylesheets/images/pdf.png';
+						$icon = $bef.$src.$aft;
+						?>
+						<a href="<?php $link; ?>" title="Download as PDF">
+							<?php echo $icon; ?>
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
+		
 		<div class="row">
 			<h5>Working to develop tools for environmental education and advocacy using open source software that leverage the power of online social media and promotes a re-engagement with the natural world.</h5>
 		</div>

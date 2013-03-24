a59558ef13669c8e8d516db9d20c87e3a9ad06a1
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-23 22:45:42 -0400

    flexslider settins for thumbnav

--- javascripts/flexslider-init.js
+++ javascripts/flexslider-init.js
@@ -2,10 +2,11 @@
     $(window).load(function() {
         $('.flexslider').flexslider({
         		slideshow: false,
+        		controlNav: "thumbnails",
 	            animation: 'slide',
-	            controlNav: true,               //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
 				directionNav: true,             //Boolean: Create navigation for previous/next navigation? (true/false)
 		    	controlsContainer: '.flex-nav'
+		    	
 	    });
     });
 })(jQuery)

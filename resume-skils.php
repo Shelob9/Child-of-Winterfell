2d9b7867773bacb400143e89e5e50639b533aa1d
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-24 02:02:25 -0400

    resume-skills template_part

---
+++ resume-skils.php
@@ -0,0 +1,9 @@
+<ul>
+	<li><strong>Environmental Studies-</strong> Trans-displinary education focusing on how behaviors and practices do and do not change with regard to environmentalism.</li>
+	<li><strong>Web Design-</strong> WordPress theme customization using HTML, CSS, PHP, jQuery and Git. Site, project and content management.</li>
+	<li><strong>Social Media-</strong> Experienced with major networks and social media management systems such as Hootsuite, Bootlenose, and Buffer.</li>
+	<li><strong>Multimedia Production-</strong> Extensive experience with audio production and editing for music and video. Proficient with Adobe Photoshop and Illustrator.</li>
+	<li><strong>Writing-</strong> Studied fiction, journalism and creative non-fiction. Published articles on various oonline publications on subjects including social media, science fiction to canine PTSD.</li>
+	<li><strong>Computers-</strong> Highly proficient with Mac OS X including networking setup and basic hardware repair and servicing. Comfortable with basic Linux server management and setup via cPanel or terminal.</li>
+	<li><strong>Marketing-</strong> Experience includes on-campus marketing on behalf of Apple computers, and a recording studio as well as social marketing for a podcast. Also experienced with affiliate marketing and Google AdWords.</li>
+</ul>

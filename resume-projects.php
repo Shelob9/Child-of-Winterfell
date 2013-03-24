c5fdab28b6486d856a8e79a35d5c56c54253e97c
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-24 02:00:29 -0400

    did resume-projects

--- resume-projects.php
+++ resume-projects.php
@@ -4,80 +4,99 @@ $aft = "\">";
 
 ?>
 <div class="twelve columns centered">
+<!--Nature Scholar-->
 	<div class="row">
-		<div class="two columns res-sq" style="margin-top:6px;">
+		<div class="three columns res-rec" >
 			<?php
-			$sqsrc = site_url('assets/gd-sq.jpg');
-			$sq = $bef.$sqsrc.$aft;
-			echo $sq;
+			$s = site_url('assets/naturescholar-300x150.jpg');
+			$src = site_url('assets/naturescholar.jpg');
+			$l = $bef.$src.$aft;
+			
 			?>
+			<a id="single_image" class="group" rel="gallery1" href="<?php echo $s; ?>" title="Nature Scholar: A guide for students of the environment."> 
+							<?php echo $l; ?>
+						</a>
 			
 		</div>
-		<div class="ten columns" >
+		<div class="nine columns" >
 			<div class="row">
 				<div class="ten columns">
-				Masters of Arts Candidate in Environmental Studies
+				<a href="http://NatureSchoalr.com" title="Nature Schoalr: Guide for student of the environment">NatureScholar.com</a>
+				</div>
+				<div class="two columns" style="text-align:right;" >
+				2012-Present
+				</div>
+			</div>
+				<ul style="margin-left:30px;">
+					<li>Co-founder, web designer for environmental education website.</li>
+					<li>Designed custom WordPress theme including author profit-sharing system utilizing Google AdWords and Amazon Associates.</li>
+					<li>Responsible for contributing original content as well as soliciting content from freelancer writers.</li>
+				</ul>
+				<em>Site is currently in soft-launch phase while we seek funding and additional content.</em>
+		</div>
+	</div>
+<!--Winterfell-->	
+	<div class="row">
+		<div class="three columns res-rec" >
+			<?php
+			$s = site_url('assets/wf900-300x211.png');
+			$src = site_url('assets/wf900.png');
+			$l = $bef.$src.$aft;
+			
+			?>
+			<a id="single_image" class="group" rel="gallery1" href="<?php echo $s; ?>" title="Winterfell Responsive WordPress Theme"> 
+							<?php echo $l; ?>
+						</a>
+			
+		</div>
+		<div class="nine columns" >
+			<div class="row">
+				<div class="ten columns">
+				<?php $cat = get_category_link( '7' ); ?> 
+				<a href="<?php echo $cat; ?>" title="WordPress">WordPress Theme Development and Site Management</a>
 				</div>
 				<div class="two columns" style="text-align:right;" >
 				2011-Present
 				</div>
 			</div>
 				<ul style="margin-left:30px;">
-					<li>Studied green architectural theory, cognitive psychology, evolutionary biology, social organization and environmental education.</li>
-					<li>Currently preparing thesis with expected graduation date of August 2013.</li>
-					<li>Member of student council.</li>
+					<li>Developing WordPress themes, <a href="http://complexwaveform.com/jp/winterfell" title="Winterfell WordPress Theme">"Winterfell"</a> and "Second Foundation." </li>
+					<li>Developed and managed <a href="http://LorenPaulCaplin.com" title="Loren-Paul Caplin">portfolio</a> and <a href="http://NewYorkScriptConsultants.com" title="New York Script Consultants">consulting</a> sites for writer, musician and professor Loren-Paul Caplin.</li>
+					<li>Member of <a href="http://make.wordpress.org/themes/" title="WordPress Theme Review Team">Theme Review Team</a> for <a href="http://WordPress.org" title="WordPress.org">WordPress.org</a>.</li>
 				</ul>
+				<em><a href="http://themes.trac.wordpress.org/query?owner=shelob9&status=accepted&status=assigned&status=closed&status=new&status=reopened&col=id&col=summary&col=status&col=type&col=priority&col=time&col=changetime&order=priority">See my theme review tickets.</a></em>
 		</div>
 	</div>
-	
+<!--Ecohyphen-->	
 	<div class="row">
-		<div class="two columns res-sq" style="margin-top:6px;">
+		<div class="three columns res-rec" >
 			<?php
-			$sqsrc = site_url('assets/ns-sq.jpg');
-			$sq = $bef.$sqsrc.$aft;
-			echo $sq;
+			$s = site_url('assets/banner-turtle-300x100.png');
+			$src = site_url('assets/banner-turtle.png');
+			$l = $bef.$src.$aft;
+			
 			?>
+			<a id="single_image" class="group" rel="gallery1" href="<?php echo $s; ?>" title="Winterfell Responsive WordPress Theme"> 
+							<?php echo $l; ?>
+						</a>
 			
 		</div>
-		<div class="ten columns" >
+		<div class="nine columns" >
 			<div class="row">
 				<div class="ten columns">
-				Bachelor of the Arts in Environmental Studies			                      
+				<a href="http://Ecohyphen.com" title="Ecohyphen: eco-consiousness, eco-philosophy, eco-humor.">Ecohyphen</a>
 				</div>
 				<div class="two columns" style="text-align:right;" >
-				2008-2010
+				2012-Present
 				</div>
 			</div>
 				<ul style="margin-left:30px;">
-					<li>Studied ecology, chemistry, physics and environmental policy.</li>
-					<li>Completed fieldwork assisting research on Million Trees NYC, and thesis on mental models and climate change.</li>
-					<li>Participated in fiction and creative non-fiction writing workshops.</li>
+					<li>Developed a podcast about sustainability and eco-philosophy.</li>
+					<li>Responsible for co-hosting and editing shows on weekly basis.</li>
+					<li>Designed website and social media marketing system.</li>
+					<li>Currently developing additional content for public radio.</li>
 				</ul>
 		</div>
-	</div>
+	</div>	
 	
-	<div class="row">
-		<div class="two columns res-sq" style="margin-top:6px;">
-			<?php
-			$sqsrc = site_url('assets/fs-sq.jpg');	
-			$sq = $bef.$sqsrc.$aft;
-			echo $sq;
-			?>
-		</div>
-		<div class="ten columns" >
-			<div class="row">
-				<div class="ten columns">
-				Associate of Science in Recording Arts			                      
-				</div>
-				<div class="two columns" style="text-align:right;" >
-				2008-2010
-				</div>
-			</div>
-				<ul style="margin-left:30px;">
-					<li>Studied audio engineering for music, film, and game production.</li>
-					<li>Studied music theory, history and business.</li>
-					
-				</ul>
-		</div>
-	</div>
 </div>

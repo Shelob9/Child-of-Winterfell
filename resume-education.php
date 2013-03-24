4e284997c12fc3d9ea3a382455ecf3b954c639e6
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-24 00:10:26 -0400

    got goddard row really correct(ish)

--- resume-education.php
+++ resume-education.php
@@ -5,7 +5,7 @@ $aftz = "\" width=\"150\" height=\"150\">";
 ?>
 <div class="twelve columns centered">
 	<div class="row">
-		<div class="two columns res-sq">
+		<div class="two columns res-sq" style="margin-top:6px;">
 			<?php
 			$sqsrc = site_url('assets/gd-sq.jpg');
 		
@@ -15,9 +15,15 @@ $aftz = "\" width=\"150\" height=\"150\">";
 			<p style="text-align:center;">Goddard College<br />
 			Plainfield, VT</p>
 		</div>
-		<div class="ten columns">
-			<h6>Masters of Arts Candidate in Environmental Studies</h6>
-			<h5>2011-Present</h5>
+		<div class="ten columns" >
+			<div class="row">
+				<div class="ten columns">
+				Masters of Arts Candidate in Environmental Studies
+				</div>
+				<div class="two columns" style="margin-bottom:13px;">
+				2011-Present
+				</div>
+			</div>
 				<ul>
 					<li>Studied green architectural theory, cognitive psychology, evolutionary biology, social organization and environmental education.</li>
 					<li>Currently preparing thesis with expected graduation date of August 2013.</li>

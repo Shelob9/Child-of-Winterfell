5c5b94b71053c3e3219668a15d12c6b9c0b6cf6f
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-23 21:27:23 -0400

    emptying resume temp

--- resume_temp.php
+++ resume_temp.php
@@ -3,24 +3,16 @@
  * Template Name: Resume Template
  */
 get_header(); ?>
+<header>
+
+</header>
+
+<!-- Main Content -->
+<div class="twelve columns" role="content">
 
 
-    <!-- Main Content -->
-    <div class="twelve columns" role="content">
-<div class="four columns">
-			<?php $src = site_url('/assets/sitting@goddard.jpg', 'http'); ?>
-			<img src="<?php echo $src; ?>" class="radi-only">
-		</div>
-		<div class="eight columns" >
-		<?php if ( have_posts() ) : ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'page' ); ?>
-			<?php endwhile; ?>
-			
-		<?php endif; ?>
-		</div>
-    </div>
-    <!-- End Main Content -->
+</div>
+<!-- End Main Content -->
 </div>
 <?php get_footer(); ?>

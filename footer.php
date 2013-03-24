d6e9edac6d16b38a8b0c0c17af1d93ed1f41d9a1
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-23 20:03:47 -0400

    Revert "footer mobile grid"
    This reverts commit a65c7b39b6dc8798423cfb521e8534059aa901aa.

--- footer.php
+++ footer.php
@@ -21,32 +21,26 @@
 	
 	<div class="row">
 		<div class=" ten columns centered total-wrap" style="padding-top:4px; padding-bottom:4px;">
-			<ul class="block-grid four-up mobile-two-up">
-				<li>
+			<div class="row">
+				<div class="two columns" id="twitter">
 					<a href="https://twitter.com/Josh412" class="twitter-follow-button" data-show-count="false" data-size="large" data-dnt="true">Follow @Josh412</a>
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-				</li>
-				<li>&nbsp;</li>
-				
-				<li>
+				</div>
+				<div class="four columns ">&nbsp;
+				</div>
+				<div class="three columns mobile-two push-two-mobile">
 					<a href="mailto:JPollock412@gmail.com">
 						<img src="<?php $src = get_stylesheet_directory_uri().'/stylesheets/images/fc-webicon-mail-m.png'; echo $src; ?>" >
 					</a>
-					<a href="21and24.tumblr.com">
-						<img src="<?php $src = get_stylesheet_directory_uri().'/stylesheets/images/fc-webicon-tumblr-m.png'; echo $src; ?>" >
-					</a>
-				
 					<a href="https://plus.google.com/u/0/108295629672902361491" rel="me">
 						<img src="<?php $src = get_stylesheet_directory_uri().'/stylesheets/images/fc-webicon-googleplus-m.png'; echo $src; ?>" >
 					</a>
-					<a href="21and24.tumblr.com">
-						<img src="<?php $src = get_stylesheet_directory_uri().'/stylesheets/images/fc-webicon-linkedin-m.png'; echo $src; ?>" >
-					</a>
-				</li>
-			
-				
-				
-			</ul>
+				</div>
+				<div class="two columns extra-button" style="margin-top:4px; margin-right:14px;">
+<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+<script type="IN/MemberProfile" data-id="http://www.linkedin.com/pub/josh-pollock/5/900/978" data-format="hover" data-text="Josh Pollock" data-related="false"></script>
+				</div>
+			</div>
 		</div>
 	</div>
 		

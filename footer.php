ef957c0e96a52b3a0816faa9016f99ea22967b52
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-23 20:08:10 -0400

    footer perfect for large no mobile

--- footer.php
+++ footer.php
@@ -22,24 +22,27 @@
 	<div class="row">
 		<div class=" ten columns centered total-wrap" style="padding-top:4px; padding-bottom:4px;">
 			<div class="row">
-				<div class="two columns" id="twitter">
+				<div class="two columns" id="twitter" style="margin-left:50px;">
 					<a href="https://twitter.com/Josh412" class="twitter-follow-button" data-show-count="false" data-size="large" data-dnt="true">Follow @Josh412</a>
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 				</div>
-				<div class="four columns ">&nbsp;
+				<div class="four columns">&nbsp;
 				</div>
-				<div class="three columns mobile-two push-two-mobile">
+				<div class="three columns">
+					<a href="mailto:JPollock412@gmail.com">
+						<img src="<?php $src = get_stylesheet_directory_uri().'/stylesheets/images/fc-webicon-tumblr-m.png'; echo $src; ?>" >
+					</a>
 					<a href="mailto:JPollock412@gmail.com">
 						<img src="<?php $src = get_stylesheet_directory_uri().'/stylesheets/images/fc-webicon-mail-m.png'; echo $src; ?>" >
 					</a>
 					<a href="https://plus.google.com/u/0/108295629672902361491" rel="me">
 						<img src="<?php $src = get_stylesheet_directory_uri().'/stylesheets/images/fc-webicon-googleplus-m.png'; echo $src; ?>" >
 					</a>
+					<a href="https://plus.google.com/u/0/108295629672902361491" rel="me">
+						<img src="<?php $src = get_stylesheet_directory_uri().'/stylesheets/images/fc-webicon-linkedin-m.png'; echo $src; ?>" >
+					</a>
 				</div>
-				<div class="two columns extra-button" style="margin-top:4px; margin-right:14px;">
-<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
-<script type="IN/MemberProfile" data-id="http://www.linkedin.com/pub/josh-pollock/5/900/978" data-format="hover" data-text="Josh Pollock" data-related="false"></script>
-				</div>
+				
 			</div>
 		</div>
 	</div>

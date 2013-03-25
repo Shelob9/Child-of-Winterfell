7b700bdfd1a103901773bd40f67b9464fa11210b
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-25 05:37:23 -0400

    fix link to post 18

--- footer.php
+++ footer.php
@@ -19,14 +19,14 @@
 					<a href="https://twitter.com/Josh412" class="twitter-follow-button" data-show-count="false" data-size="large" data-dnt="true">Follow @Josh412</a>
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 					<a href="mailto:JPollock412@gmail.com" class="genericon genericon-mail" style="font-size:32px;margin-right:16px;"></a>
-					<a href="21and24.tumblr.com" title="My tumblr of nature photos" class="genericon genericon-tumblr" style="font-size:32px;margin-right:16px;"></a>
+					<a href="http://21and24.tumblr.com" title="My tumblr of nature photos" class="genericon genericon-tumblr" style="font-size:32px;margin-right:16px;"></a>
 					<a href="https://plus.google.com/u/0/108295629672902361491" title="Google Plus Profile" class="genericon genericon-googleplus" style="font-size:32px;margin-right:16px;" rel="me"></a>
 					<a href="http://www.linkedin.com/pub/josh-pollock/5/900/978" class="genericon genericon-linkedin-alt" title="Linkedin Profile" style="font-size:32px;margin-right:16px;"></a>
 				</div>
 				
 				<div class="mobile-four show-for-small" style="text-align:center;">
 					<a href="mailto:JPollock412@gmail.com" class="genericon genericon-mail" ></a>
-					<a href="21and24.tumblr.com" title="My tumblr of nature photos" class="genericon genericon-tumblr"></a>
+					<a href="http://21and24.tumblr.com" title="My tumblr of nature photos" class="genericon genericon-tumblr"></a>
 					<a href="https://plus.google.com/u/0/108295629672902361491" title="Google Plus Profile" class="genericon genericon-googleplus" rel="me"></a>
 					<a href="http://www.linkedin.com/pub/josh-pollock/5/900/978" class="genericon genericon-linkedin-alt" title="Linkedin Profile"></a>
 					<a href="http://twitter.com/Josh412" class="genericon genericon-twitter"></a>

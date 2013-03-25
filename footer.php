a48bbd5dbac0e6020233d9f59257e0f2f143e5e8
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-24 22:35:40 -0400

    fixed footer for ipad screen

--- footer.php
+++ footer.php
@@ -15,12 +15,15 @@
 	<div class="row">
 		<div class=" ten columns centered total-wrap" style="padding-top:4px; padding-bottom:4px;">
 			<div class="row">
-				<div class="two columns hide-for-small" id="twitter" style="margin-left:50px;">
+				<div class="six offset-by-four centered hide-for-small">
 					<a href="https://twitter.com/Josh412" class="twitter-follow-button" data-show-count="false" data-size="large" data-dnt="true">Follow @Josh412</a>
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+					<a href="mailto:JPollock412@gmail.com" class="genericon genericon-mail" style="font-size:32px;margin-right:16px;"></a>
+					<a href="21and24.tumblr.com" title="My tumblr of nature photos" class="genericon genericon-tumblr" style="font-size:32px;margin-right:16px;"></a>
+					<a href="https://plus.google.com/u/0/108295629672902361491" title="Google Plus Profile" class="genericon genericon-googleplus" style="font-size:32px;margin-right:16px;" rel="me"></a>
+					<a href="http://www.linkedin.com/pub/josh-pollock/5/900/978" class="genericon genericon-linkedin-alt" title="Linkedin Profile" style="font-size:32px;margin-right:16px;"></a>
 				</div>
-				<div class="four columns hide-for-small">&nbsp;
-				</div>
+				
 				<div class="mobile-four show-for-small" style="text-align:center;">
 					<a href="mailto:JPollock412@gmail.com" class="genericon genericon-mail" ></a>
 					<a href="21and24.tumblr.com" title="My tumblr of nature photos" class="genericon genericon-tumblr"></a>
@@ -29,13 +32,9 @@
 					<a href="http://twitter.com/Josh412" class="genericon genericon-twitter"></a>
 				</div>
 					
-				<div class="three columns hide-for-small">
 				
-					<a href="mailto:JPollock412@gmail.com" class="genericon genericon-mail" style="font-size:32px;margin-right:16px;"></a>
-					<a href="21and24.tumblr.com" title="My tumblr of nature photos" class="genericon genericon-tumblr" style="font-size:32px;margin-right:16px;"></a>
-					<a href="https://plus.google.com/u/0/108295629672902361491" title="Google Plus Profile" class="genericon genericon-googleplus" style="font-size:32px;margin-right:16px;" rel="me"></a>
-					<a href="http://www.linkedin.com/pub/josh-pollock/5/900/978" class="genericon genericon-linkedin-alt" title="Linkedin Profile" style="font-size:32px;margin-right:16px;"></a>
-				</div>
+				
+					
 				
 			</div>
 		</div>

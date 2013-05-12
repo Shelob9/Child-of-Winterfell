215c875720e1efa531c6e643ca251775b1df5c27
Josh <JPollock412@gmail.com>
2013-05-12 00:40:34 -0400

    add everything for ajax page loads

--- functions.php
+++ functions.php
@@ -39,5 +39,28 @@ add_action( 'wp_enqueue_scripts', 'cwf_scriptStyles', 20 );
 		add_image_size( '500sq',  500, 500 );
 		add_image_size( '600sq',  600, 600);
 		
-		
- ?>
+/*
+ * AJAX page Loads
+ * Method from: http://wptheming.com/2011/12/ajax-themes/
+ *
+ * https://github.com/balupton/History.js
+ *
+ */
+ 
+function _sf_ajax_page_load() {
+		if ( get_theme_mod( '_sf_ajax' ) == '' ) {
+			if ( !is_admin() ) :
+				wp_deregister_script('historyjs');
+				wp_register_script( 'historyjs', get_stylesheet_directory_uri(). '/javascripts/jquery.history.js', array( 'jquery' ), '1.7.1' );
+				wp_enqueue_script( 'historyjs' );
+				wp_register_script( 'ajax_init', get_stylesheet_directory_uri(). '/javascripts/ajax_load.js', array( 'historyjs' ), false, true );
+				wp_enqueue_script( 'ajax_init' );
+			endif;
+		}	
+}
+
+add_action( 'wp_enqueue_scripts','_sf_ajax_page_load' );
+
+ 
+
+?>
